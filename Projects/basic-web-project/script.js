3d88e666a6d099bb56cd3f1ba3cdbbf0c3243cf6
Praful <rajputpraful791@gmail.com>
2025-11-09 22:07:56 +0530

    Adding a testing project

---
+++ Projects/basic-web-project/script.js
@@ -0,0 +1,3 @@
+document.getElementById('clickButton').addEventListener('click', function() {
+    document.getElementById('message').textContent = 'Button clicked! Welcome to the basic web project.';
+});
